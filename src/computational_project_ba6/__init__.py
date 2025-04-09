d7733bc07f04edf4e90c9a7dc02b737aa3e47919
Diane and Cedric <cedric.rossboth@epfl.ch>
2025-04-09 15:13:11 +0200

    Initial commit from https://github.com/schwallergroup/copier-liac-minimal

---
+++ src/computational_project_ba6/__init__.py
@@ -0,0 +1,5 @@
+"""Symbolic regression predicting spin splitting energy of transition metal complexes."""
+
+from __future__ import annotations
+
+__version__ = "0.0.1"
---
+++ tests/test_example_module.py
@@ -0,0 +1,6 @@
+from computational_project_ba6.example_module import hello_smiles
+
+
+# Test the function
+def test_hello_smiles():
+    assert hello_smiles("C(=O)O") == "Hello, C(=O)O!", "Test failed: SMILES input"
